0d4542c3cfe4098d23ae944bae7c5b1a7bf0b469
leonawicz <matt.rstat@gmail.com>
2017-07-08 09:45:43 -0600

    Increment package version, import rintrojs and shinytoastr, update use_apputils to optionally make use of these packages, update documentation.

--- R/apputils.R
+++ R/apputils.R
@@ -2,14 +2,25 @@
 #'
 #' Call this function once near the top of the Shiny UI definition.
 #'
-#' Calling \code{use_apputils} loads package css into the head of a shiny app.
+#' Calling \code{use_apputils} loads package css into the head of a Shiny app.
+#' Optionally calls \code{introjsUI()} and/or \code{useToastr()}, in which case the Shiny app must have these packages loaded.
+#'
+#' @param use_rintrojs logical, also loads \code{rintrojs} package UI components via \code{introjsUI}.
+#' @param use_shinytoastr logical, also loads \code{shinytoastr} package UI components via \code{useToastr}.
 #'
 #' @return HTML tags placed in the head of the app UI HTML file.
 #' @export
 #'
 #' @examples
-#' # See usage.
-use_apputils <- function() {
-  addResourcePath("resources", system.file("resources", package = "apputils"))
-  shiny::tags$head(shiny::tags$link(rel = 'stylesheet', type = 'text/css', href = 'resources/apputils.css'))
+#' #not run
+use_apputils <- function(use_rintrojs=FALSE, use_shinytoastr=FALSE) {
+  shiny::addResourcePath("resources", system.file("resources", package = "apputils"))
+  x <- shiny::tags$head(shiny::tags$link(rel = 'stylesheet', type = 'text/css', href = 'resources/apputils.css'))
+  if(!use_rintrojs & !use_shinytoaster) return(x)
+  if(use_rintrojs & use_shinytoaster)
+    return(shiny::tagList(x, shinytoastr::useToastr(), rintrojs::introjsUI()))
+  if(use_rintrojs)
+    return(shiny::tagList(x, rintrojs::introjsUI()))
+  if(use_shinytoaster)
+    return(shiny::tagList(x, shinytoastr::useToastr()))
 }
