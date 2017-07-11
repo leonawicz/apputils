ed38ebf9c8f499cd4edbaaeef6748299cba8de21
leonawicz <matt.rstat@gmail.com>
2017-07-10 19:42:46 -0600

    Generalize argument.

--- R/plots.R
+++ R/plots.R
@@ -31,21 +31,21 @@ tolpal <- function(n){
 #' when there are too many points to plot.
 #'
 #' @param x special data frame.
-#' @param cru character, name of CRU data set.
+#' @param baseline character, name of baseline data set.
 #'
 #' @return a number.
 #' @export
 #'
 #' @examples
 #' #not run
-samplesize_factor <- function(x, cru){
+samplesize_factor <- function(x, baseline){
   if("RCP" %in% names(x)){
     n.rcps <- nlevels(x$RCP)
     if("Historical" %in% levels(x$RCP) && n.rcps > 1) n.rcps <- n.rcps - 1
   } else n.rcps <- 1
   if("Model" %in% names(x)){
     n.models <- nlevels(x$Model)
-    if(cru %in% levels(x$Model) && n.models > 1) n.models <- n.models - 1
+    if(baseline %in% levels(x$Model) && n.models > 1) n.models <- n.models - 1
   } else n.models <- 1
   n.rcps * n.models
 }
