7518c113d98baa1114cff9d7370f78b57cfc3a6e
leonawicz <matt.rstat@gmail.com>
2017-09-01 08:44:31 -0600

    Generalize function, move SNAP-specific wrapper to snaputils.

--- R/info.R
+++ R/info.R
@@ -14,58 +14,40 @@
 }
 # nolint end
 
-.app_img_links <- function(app_url, img_url, title, subtitle, drop=NULL,
-                           height=200, min.width=300, max.width=400, col.width=4){
+.app_img_links <- function(app_url, img_url, title, subtitle, drop = NULL,
+                           height = 200, min_width = 300, max_width = 400, col_width = 4){
   apps <- basename(app_url)
-  if(!is.null(drop) && !all(drop %in% apps)) stop("Cannot drop apps that are not in the master list.")
+  if(!is.null(drop) && !all(drop %in% apps))
+    stop("Cannot drop apps that are not in the master list.")
   idx <- if(is.null(drop)) seq_along(apps) else seq_along(app_url)[-match(drop, apps)]
-  x <- purrr::map(idx,
-                  ~shiny::column(col.width, .app_img_link(app_url[.x], img_url[.x], title[.x], subtitle[.x], height),
-                          style=paste0("min-width: ", min.width, "px; max-width: ", max.width, "px; padding:5px;")))
-  shiny::fluidRow(x, style="padding: 10px;")
+  x <- purrr::map(
+    idx,  ~shiny::column(
+      col_width, .app_img_link(app_url[.x], img_url[.x], title[.x], subtitle[.x], height),
+      style = paste0("min-width: ", min_width, "px; max-width: ", max_width, "px; padding:5px;")))
+  shiny::fluidRow(x, style = "padding: 10px;")
 }
 
 #' Genrate app showcase content
 #'
-#' @param drop character vector of any apps to exclude from showcase.
+#' @param app_url character vector of app urls.
+#' @param img_url character vector of image urls, same length as \code{app_url}.
+#' @param title character, titles displayed on image links during mouseover.
+#' @param subtitle character, subtitles displayed on image link during mouseover.
+#' @param drop character vector of any apps to exclude from showcase. Useful for programmed app lists that may include self.
+#' @param height numeric, height in pixels.
+#' @param min_width numeric, minimum width in pixels.
+#' @param max_width numeric, maximum width in pixels.
+#' @param col_width integer, column width number for row, 1 through 12, defaults to 4.
 #'
 #' @return a shiny fluidRow containing organized and stylized app image links for reference.
 #' @export
 #'
 #' @examples
 #' #not run
-app_showcase <- function(drop=NULL){
-  args <- list(
-    app_url=c(
-      "http://shiny.snap.uaf.edu/climdist",
-      "https://uasnap.shinyapps.io/jfsp-v10",
-      "https://uasnap.shinyapps.io/ar5eval",
-      "https://uasnap.shinyapps.io/cc4liteFinal",
-      "https://uasnap.shinyapps.io/ex_leaflet",
-      "https://uasnap.shinyapps.io/nwtapp",
-      "http://shiny.snap.uaf.edu/standage"
-    ),
-    img_url=c(
-      "https://raw.githubusercontent.com/leonawicz/dash/master/images/_climdist_small.png",
-      "https://raw.githubusercontent.com/leonawicz/jfsp/master/_jfsp_small.png",
-      "https://raw.githubusercontent.com/leonawicz/ar5eval/master/_ar5eval_small.png",
-      "https://github.com/ua-snap/shiny-apps/raw/master/_images/small/cc4liteFinal.jpg",
-      "https://github.com/ua-snap/shiny-apps/raw/master/_images/small/ex_leaflet.jpg",
-      "https://github.com/ua-snap/shiny-apps/raw/master/_images/small/nwtapp.jpg",
-      "https://raw.githubusercontent.com/leonawicz/agedist/master/_agedist_small.png"
-    ),
-    title=c(
-      "CMIP5 Regional Climate", "Alaska Wildfire Projections", "Climate Model Analysis",
-      "Communities & Climate", "Interactive Documents", "Northwest Territories",
-      "Alaska Vegetation Changes"
-    ),
-    subtitle=c(
-      "Full distributions", "ALFRESCO model output", "CMIP5 GCM evaluation",
-      "Alaska & western Canada", "Leaflet + Shiny observers", "Climate projections",
-      "Tree stand age projections"
-    )
-  )
-  do.call(.app_img_links, c(args, drop=list(drop)))
+app_showcase <- function(app_url, img_url, title, subtitle, drop = NULL, height = 200,
+                         min_width = 300, max_width = 400, col_width = 4){
+  .app_img_links(app_url, img_url, title, subtitle, height,
+                 min_width, max_width, col_width)
 }
 
 #' Generate a recommended app citation
