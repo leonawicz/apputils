235faad0ba0b9ce56ac27e396a68f46b7964d8c9
leonawicz <matt.rstat@gmail.com>
2017-10-25 14:03:54 -0600

    Update globals. Update use_apputils with new CSS argument. Remove imports. Tidy code.

--- R/apputils.R
+++ R/apputils.R
@@ -1,4 +1,4 @@
-globalVariables(c(".", "Model", "RCP", "Year", "Var", "Val", "Decade", "Decadal_mean", "input"))
+globalVariables(c(".data"))
 
 #' The \code{apputils} package contains common utility functions, settings and references for development use across multiple Shiny apps.
 #'
@@ -22,9 +22,6 @@ globalVariables(c(".", "Model", "RCP", "Year", "Var", "Val", "Decade", "Decadal_
 NULL
 
 #' @importFrom magrittr %>%
-#' @importFrom grDevices dev.off
-#' @importFrom graphics abline arrows axis barplot boxplot hist legend lines par plot points polygon segments text
-#' @importFrom stats dnorm qnorm rnorm lm
 NULL
 
 #' Initialize apputils
@@ -36,15 +33,27 @@ NULL
 #'
 #' @param use_rintrojs logical, also loads \code{rintrojs} package UI components via \code{introjsUI}.
 #' @param use_shinytoastr logical, also loads \code{shinytoastr} package UI components via \code{useToastr}.
+#' @param force_AdminLTE logical, force load \code{AdminLTE.css} from \code{shinydashboard} even if the app does not use a dashboard layout.
 #'
 #' @return HTML tags placed in the head of the app UI HTML file.
 #' @export
 #'
 #' @examples
-#' #not run
-use_apputils <- function(use_rintrojs=FALSE, use_shinytoastr=FALSE) {
+#' \dontrun{
+#' use_apputils()
+#' use_apputils(force_AdminLTE = TRUE)
+#' use_apputils(use_rintrojs = TRUE, use_shinytoastr = TRUE)
+#' }
+use_apputils <- function(use_rintrojs = FALSE, use_shinytoastr = FALSE, force_AdminLTE = FALSE) {
   shiny::addResourcePath("resources", system.file("resources", package = "apputils"))
-  x <- shiny::tags$head(shiny::tags$link(rel = "stylesheet", type = "text/css", href = "resources/apputils.css"))
+  if(force_AdminLTE)
+    shiny::addResourcePath("AdminLTE", system.file("AdminLTE", package = "shinydashboard"))
+
+  x <- shiny::tags$head(shiny::tags$link(
+    rel = "stylesheet", type = "text/css", href = "resources/apputils.css"))
+  if(force_AdminLTE) x <- shiny::tagList(shiny::tags$link(
+    rel = "stylesheet", type = "text/css", href = "AdminLTE/AdminLTE.css"), x)
+
   if(!use_rintrojs & !use_shinytoastr) return(x)
   if(use_rintrojs & use_shinytoastr)
     return(shiny::tagList(shinytoastr::useToastr(), rintrojs::introjsUI(), x))
@@ -75,7 +84,7 @@ use_apputils <- function(use_rintrojs=FALSE, use_shinytoastr=FALSE) {
 #' @param container list of style arguments for the container div. See details and example.
 #' @param toast list of style arguments for the toast. See details and example.
 #' @param position character, defaults to \code{"top-center"}.
-#' @param rgba numeric, vector of four css rgba property values for background color, e.g., \code{c(0,0,0,0.5)}. See details.
+#' @param rgba numeric, vector of four css rgba property values for background color, e.g., \code{c(0, 0, 0, 0.5)}. See details.
 #' @param hover.rgba numeric, vector of four css rgba property values for background color on mouse hover. See details.
 #' @param opacity numeric, toast opacity. Appended to \code{container}.
 #' @param hover.opacity numeric, toast opacity on mouse hover.
@@ -86,17 +95,18 @@ use_apputils <- function(use_rintrojs=FALSE, use_shinytoastr=FALSE) {
 #'
 #' @examples
 #' update_toastr_css(
-#'   list('overflow-y'='auto', width='70%', height='700px'),
-#'   list(top='100px', margin='0 auto', left='115px')
+#'   list('overflow-y' = 'auto', width = '70%', height = '700px'),
+#'   list(top = '100px', margin = '0 auto', left = '115px')
 #' )
-update_toastr_css <- function(container=NULL, toast=NULL, rgba=NULL, hover.rgba=NULL,
-                              opacity=NULL, hover.opacity=NULL, radius=NULL, position="top-center"){
+update_toastr_css <- function(container = NULL, toast = NULL, rgba = NULL, hover.rgba = NULL,
+                              opacity = NULL, hover.opacity = NULL, radius = NULL,
+                              position = "top-center"){
   if(is.null(container) & is.null(toast))
     stop("Must provide valid arguments to at least one of container or toast.")
   set_op <- function(op){
-    list(opacity=op,
-         '-ms-filter'=paste0("progid:DXImageTransform.Microsoft.Alpha(Opacity=", 100*op, ")"),
-         filter=paste0("alpha(", 100*op, ")"))
+    list(opacity = op,
+         '-ms-filter'=paste0("progid:DXImageTransform.Microsoft.Alpha(Opacity=", 100 * op, ")"),
+         filter=paste0("alpha(", 100 * op, ")"))
   }
 
   if(!is.null(opacity) && is.null(rgba)){
@@ -106,27 +116,27 @@ update_toastr_css <- function(container=NULL, toast=NULL, rgba=NULL, hover.rgba=
   if(!is.null(hover.opacity) & is.null(hover.rgba)){
     hov.op <- set_op(hover.opacity)
     hover <- paste0('#toast-container.toast-', position, ' > :hover {\n  ',
-                    paste0(paste(names(hov.op), hov.op, sep=":", collapse=";\n  "), ";\n}"))
+                    paste0(paste(names(hov.op), hov.op, sep = ":", collapse = ";\n  "), ";\n}"))
   } else hover <- NULL
 
-  if(!is.null(rgba)) rgba <- paste0('rgba(', paste0(rgba, collapse=","), ")")
-  if(!is.null(hover.rgba)) hover.rgba <- paste0('rgba(', paste0(hover.rgba, collapse=","), ")")
-  if(!is.null(rgba)) container <- c(container, list('background-color'=rgba))
+  if(!is.null(rgba)) rgba <- paste0('rgba(', paste0(rgba, collapse = ","), ")")
+  if(!is.null(hover.rgba)) hover.rgba <- paste0('rgba(', paste0(hover.rgba, collapse = ","), ")")
+  if(!is.null(rgba)) container <- c(container, list('background-color' = rgba))
   if(!is.null(hover.rgba)){
     hover <- paste0('#toast-container.toast-', position, ' > :hover {\n  ',
-                    paste0(paste("background-color", hover.rgba, sep=":", collapse=";\n  "), ";\n}"))
+                    paste0(paste("background-color", hover.rgba, sep = ":", collapse = ";\n  "), ";\n}"))
   }
   if(!is.null(radius)) container <- c(container, list(
-    '-moz-border-radius'=radius, '-webkit-border-radius'=radius, 'border-radius'=radius))
+    '-moz-border-radius' = radius, '-webkit-border-radius' = radius, 'border-radius' = radius))
 
   if(!is.null(container))
     container <- paste0('#toast-container.toast-', position, ' > div {\n  ',
-      paste0(paste(names(container), container, sep=":", collapse=";\n  "), ";\n}"))
+      paste0(paste(names(container), container, sep = ":", collapse = ";\n  "), ";\n}"))
   if(!is.null(toast))
     toast <- paste0('.toast-', position, ' {\n  ',
-      paste0(paste(names(toast), toast, sep=":", collapse=";\n  "), ";\n}"))
+      paste0(paste(names(toast), toast, sep = ":", collapse = ";\n  "), ";\n}"))
 
-  shiny::tags$style(shiny::HTML(paste(container, toast, hover, sep="\n")))
+  shiny::tags$style(shiny::HTML(paste(container, toast, hover, sep = "\n")))
 }
 # nolint end
 
@@ -142,7 +152,7 @@ update_toastr_css <- function(container=NULL, toast=NULL, rgba=NULL, hover.rgba=
 #' @examples
 #' inputs_not_null(c("input1", "input2"))
 inputs_not_null <- function(inputs){
-  paste(paste0("input.", inputs), "!== null", collapse=" & ")
+  paste(paste0("input.", inputs), "!== null", collapse = " & ")
 }
 
 # nolint start
@@ -160,7 +170,7 @@ inputs_not_null <- function(inputs){
 #'
 #' @examples
 #' #not run
-app_overlay <- function(text, logo=NULL, loading.logo=NULL, text.loading='<h1>Loading...</h1>'){
+app_overlay <- function(text, logo = NULL, loading.logo = NULL, text.loading = '<h1>Loading...</h1>'){
   x <- '<div id="fade-wrapper">\n  <div id="fade-wrapper-content">'
   if(!is.null(logo)) x <- paste0(x, '<p><img src="', logo, '"></p><br/><br/>')
   if(!is.null(text)) x <- paste0(x, text)
